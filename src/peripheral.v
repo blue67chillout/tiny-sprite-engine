b0f413ac0e55891b7008e8d9e3796ea75b0d9d4c
blue67chillout <arteckcr.221ec224@nitk.edu.in>
2025-09-01 11:45:02 +0530

    periph fixed

--- src/peripheral.v
+++ src/peripheral.v
@@ -36,7 +36,7 @@ module tqvp_tiny_sprite_engine (
     wire visible;
     reg [9:0] pix_x;
     reg [9:0] pix_y;
-    wire sprite_pixel_on ;
+    
     wire [1:0]R,G,B ;
     
     video_controller u_video_controller(
